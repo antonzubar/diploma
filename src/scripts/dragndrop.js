347a4ecd01f1592ec222aa2a5ede2e886be892f0
antonzubar <anton-zubar@yandex.ru>
2019-06-01 13:58:30 +0300

    dragn

--- src/scripts/dragndrop.js
+++ src/scripts/dragndrop.js
@@ -1,1 +1,18 @@
 dragula([document.querySelector('#left'), document.querySelector('#right')]);
+
+elementList = document.querySelectorAll('.ittinery-list__place');
+
+elementList.forEach(myFunction);
+
+function myFunction(value) {
+    value.addEventListener("mouseup", function myFunction() {        
+    var x = document.getElementById("drag");
+    var y = document.querySelector('#right > div') !== null;
+        console.log(y);
+        if (y) {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";  
+        }
+      });
+}
