8d0c58811ee160c272049a907c25e5127b6b72b4
antonzubar <anton-zubar@yandex.ru>
2019-06-16 14:13:48 +0300

    improve search

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,20 +1,48 @@
 function mySearch() {
+
+  // Declare variables for removing previous search results 
+  var searchList = document.getElementById('searchResultList');
+  var searchListItems = document.getElementsByClassName('search-item');
+
+  // Removing previous search results
+  while (searchList.firstChild) {
+    searchList.removeChild(searchList.firstChild);
+  }
+
   // Declare variables  
-  var input, filter, ul, li, a, i, txtValue;
+  var input, filter, ul, li, a, i, txtValue, node, textnode;
   input = document.getElementById('destinationSearch');
   filter = input.value.toUpperCase();
   ul = document.getElementById("myUL");
   li = ul.getElementsByClassName('test');
 
+  if (filter == "") {
+    ul.style.display = "";
+  } else {
+    ul.style.display = "none";
+    }
+
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName('span')[0];
     txtValue = a.textContent || a.innerText;
-    console.log(txtValue);
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
+      node = document.createElement("LI");
+      node.className = "search-item col-5";
+      textnode = document.createTextNode(txtValue);
+      node.appendChild(textnode);
+      document.getElementById("searchResultList").appendChild(node);
     } else {
       li[i].style.display = "none";
     }
+  }
+
+  if (searchListItems.length == 0){
+      node = document.createElement("LI");
+      node.className = "no-results col-5";
+      textnode = document.createTextNode("Sorry, there are no results :(");
+      node.appendChild(textnode);
+      document.getElementById("searchResultList").appendChild(node);
   }
 }
